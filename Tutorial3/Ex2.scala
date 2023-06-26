28a30e38d6df1db225fb05273f515ad3dafc16fa
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2023-06-26 20:32:37 +0530

    Delete Tutorial3 directory

--- Tutorial3/Ex2.scala
+++
@@ -1,8 +0,0 @@
-object Ex2 extends App{
-
-  def filterStrings(list: List[String]):List[String]=list.filter( _.length>5);
-
-  val inputList=List("apple","banana","kiwi","Pineapple","Orange");
-  val outList=filterStrings(inputList);
-  println(outList)
-}
